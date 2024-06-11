c74a03b9848207e67a91eced62dcf152350ee98a
ankitkaushikx <ankitkaushikx@gmail.com>
2024-06-11 23:36:21 +0530

    Layout Componets Created. Infilled into welcome

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -1,13 +1,6 @@
-<!DOCTYPE html>
-<html lang="en">
-<head>
-    <meta charset="UTF-8">
-    <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <meta http-equiv="X-UA-Compatible" content="ie=edge">
-    <title>{{env('APP_NAME')}}</title>
-    @vite(['resources/css/app.css', 'resources/js/app.css'])
-</head>
-<body>
-   <h1 class="text-4xl text-emerald-600">dd</h1>
-</body>
-</html>
+<x-layout>
+    <h2>Welcome</h2>
+    <x-demo>
+        
+    </x-demo>
+</x-layout>
